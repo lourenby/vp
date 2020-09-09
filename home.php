f37e3cfb11b375cc4322418ef5a6f1e9716d0756
Laura Paulman <lourenby@users.noreply.github.com>
2020-09-09 15:40:57 +0300

    teise tunni failid

---
+++ home.php
@@ -0,0 +1,39 @@
+<?php
+$username = "Laura Paulman";
+$fulltimenow = date("d.m.Y H:i:s");
+$hournow = date("H");
+$partofday = "lihtsalt aeg";
+if($hournow < 7){
+	$partofday = "uneaeg";
+}
+if($hournow >= 8 and $hournow < 18) {
+	$partofday = "akadeemilise aktiivsuse aeg";
+}
+
+//vaatame semestri kulgemist
+$semesterstart = new DateTime("2020-8-31");
+$semesterend = new DateTime("2020-12-13");
+//selgitame välja nende vahe ehk erinevuse
+$semestereduration = $semesterstart->diff($semesterend);
+$semesterdurationdays = $semesterduration->format("%r%a");
+
+//tänane päev 
+$today = new DateTime("now");
+
+//if($fromsemesterstartdays <0) {semester pole peale hakkanud}
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $username; ?>Laura Paulman programmeerib veebi</title>
+  
+</head>
+<body>
+  <h1><?php echo $username; ?></h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>Leht on loodud veebiprogrammeerimise kursuse raames <a href="http://www.tlu.ee">Tallinna ülikooli</a> Digitehnoloogiate instituudis.</p>
+  <p>Lehe avamise hetkel oli: <?php echo $fulltimenow ?>.</p>
+  <p><?php echo "Parajasti on " .$partofday . "."; ?></p>
+</body>
+</html>
